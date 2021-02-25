9fea41f58c0ef888f9dae03b851e37d23a421f57
harmony222 <scarleth@oregonstate.edu>
2021-02-25 15:41:39 -0800

    user can navigate to movie details from superhero grid view

--- flixy/MovieGridViewController.swift
+++ flixy/MovieGridViewController.swift
@@ -44,9 +44,7 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
                 // store movies in an array to make available to other functions
                 self.movies = dataDictionary["results"] as! [[String:Any]]
                 self.collectionView.reloadData()
-                print(self.movies)
 
-//                print(dataDictionary)
             }
         }
         task.resume()
@@ -71,14 +69,23 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
     }
     
 
-    /*
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        let cell = sender as! UICollectionViewCell
+        let indexPath = collectionView.indexPath(for: cell)!
+        let movie = movies[indexPath.item]
+        
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        collectionView.deselectItem(at: indexPath, animated: true)
     }
-    */
+
 
 }
