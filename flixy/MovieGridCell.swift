cfd413029628c0d312a4fa8e4b86d129ad00af25
harmony222 <scarleth@oregonstate.edu>
2021-02-25 15:04:05 -0800

    user can view movie details, user can view superhero movies in grid display

---
+++ flixy/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  flixy
+//
+//  Created by Harmony Scarlet on 2/25/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+
+
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
